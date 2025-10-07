053c2bb904bd1a4b92910e0d1209168a2b6e3cc0
discon33ct <maciejjajko24@gmail.com>
2025-10-07 11:23:32 +0200

    zadanie

--- src/RunInfo.java
+++ src/RunInfo.java
@@ -4,6 +4,8 @@ public class RunInfo {
         System.out.println("Instrukcja");  // usuń tę linię kodu
         System.out.println("Info dla użytkownika");
 //        na branchu o nazwie 'task' popraw następną linijkę dopisując numer kolejny komputera
-
+        System.out.println("Pracuję na komputerze o numerze 11 w sali 208 w Zespole Szkół im. ks. Stanisława Staszica w Tarnobrzegu");
+//        na branchu o nazwie 'task' w następnej linii dopisz miejscowość zamieszkania
+        System.out.println("Mieszkam w Tarnobrzegu");
     }
 }
